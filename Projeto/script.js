5c504f0d8ce6088adf700a8427fd74e91c85fa4c
odntht <odntht@gmail.com>
2019-08-14 18:01:02 -0300

    UPDATE - Finalizado slide Engine e criando slide Color

--- Projeto/script.js
+++ Projeto/script.js
@@ -24,7 +24,7 @@ function exibirEngine(){
 $("#exibirCor").click(() => ocultaEngine());
 
 function ocultaEngine() {
-    $("#slideEng").fadeOut("slow", exibirCor());
+  $("#slideEng").fadeOut("slow", exibirCor());
 }
 
 function exibirCor(){
@@ -32,12 +32,24 @@ function exibirCor(){
   $("#slideCor").show();
 }
 
+$("#exibirRoda").click(()=> ocultaCor())
+
+function ocultaCor(){
+  $("#slideCor").fadeOut("slow", exibirRoda())
+}
+
+function exibirRoda(){
+  $("#slideRoda").fadeIn(3000);
+  $("slideRoda").show();
+}
+
+
 function trocaImagemEng(id){
   switch (id) {
     case "Eng75":
-      $('#slideEng div div img').attr('src',"arquivos/images/1.png");
-      $("#slideEng footer div:nth-child(1) span").html("$63.000");
-      $("#slideEng footer div:nth-child(3)").html("75 <span>P</span>");
+    $('#slideEng div div img').attr('src',"arquivos/images/1.png");
+    $("#slideEng footer div:nth-child(1) span").html("$63.000");
+    $("#slideEng footer div:nth-child(3)").html("75 <span>P</span>");
     break;
     case "Eng100":
     $('#slideEng div div img').attr('src',"arquivos/images/2.png");
@@ -50,7 +62,27 @@ function trocaImagemEng(id){
     $("#slideEng footer div:nth-child(1) span").html("$73.000");
     $("#slideEng footer div:nth-child(3)").html("125 <span>B</span>");
     break;
+
+    case "corRed":
+    $('#imgCor img').attr('src',"arquivos/images/4.png");
+    $("#imgCor p").html("Metalic Vermilion <br> <span>Included</span>");
+    $("#slideCor footer div:nth-child(1)").html("Total <br> <span>$63.000</span>");
+    break;
+
+    case "corBlue":
+    $('#imgCor img').attr('src',"arquivos/images/5.png");
+    $("#imgCor p").html("Thunderbolt Blue <br> <span>+$1.200</span>");
+    $("#slideCor footer div:nth-child(1)").html("Total <br> <span>$64.200</span>");
+    break;
+
+    case "corGrey":
+    $('#imgCor img').attr('src',"arquivos/images/6.png");
+    $("#imgCor p").html("Space Grey <br> <span>+$1.200</span>");
+    $("#slideCor footer div:nth-child(1)").html("Total <br> <span>$64.200</span>");
+    break;
+
     default:
+    // alert(id);
   }
 };
 
@@ -64,3 +96,13 @@ $(".list-group-item").click(function() {
   $("#"+this.id).addClass("active");
   trocaImagemEng(this.id);
 });
+
+$("#listCor .col-4").click(function() {
+  let irmaos = $(this).parent().children("div");
+  for (let i = 0; i < irmaos.length; i++)
+  {
+    $("#"+irmaos[i].id).removeClass("active");
+  }
+  $("#"+this.id).addClass("active");
+  trocaImagemEng(this.id);
+})
